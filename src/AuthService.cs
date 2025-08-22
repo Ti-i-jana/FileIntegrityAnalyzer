eefe9f2dfb0781d18851ef700909120d4aee1b70
Ti-i-jana <116262457+Ti-i-jana@users.noreply.github.com>
2025-08-22 18:37:50 +0200

    WIP Auth Service setup

---
+++ src/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Graph;
+using Azure.Identity;
+using System.Threading.Tasks;
+using System;
+
+static class AuthService
+{
+    public static string clientID = "9efd73c8-a015-466e-98e1-433173eeff92";
+    public static string tenantID = "common";
+    public static string[] scopes = new string[] {"User.Read","Files.ReadWrite"};
+
+    public static async Task<GraphServiceClient> GetGraphServiceClient()
+    {
+        var options = new DeviceCodeCredentialOptions
+        {
+            ClientId = clientID,
+            TenantId = tenantID,
+            DeviceCodeCallback = (code, cancellation) =>
+            {
+                Console.WriteLine(code.Message);
+                return Task.FromResult(0);
+            }
+        };
+
+        var deviceCodeCredential = new DeviceCodeCredential(options);
+
+        var graphClient = new GraphServiceClient(deviceCodeCredential, scopes);
+
+        return graphClient;
+    }
+
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var graphClient = await AuthService.GetGraphServiceClient();
+        var me = await graphClient.Me.GetAsync();
+        Console.WriteLine($"Hello, {me.DisplayName} ({me.UserPrincipalName})");
+    }
+}
